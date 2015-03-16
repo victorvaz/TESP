48adb3570f9f4875e9ab96da632540cfe9f28906
Víctor Vaz <victor-vaz@hotmail.com>
2015-03-16 13:37:31 -0300

    Atualização 16/03/2015

---
+++ src/tesp/victor/entidade/Aluno.java
@@ -0,0 +1,50 @@
+package tesp.victor.entidade;
+
+import java.util.Date;
+
+public class Aluno extends Pessoa {
+
+    private Long matricula;
+    private Date dataAniversario;
+
+    public Aluno(Long id, String nome, String cpf, Long matricula, Date dataAniversario) {
+        super(id, nome, cpf);
+        this.matricula = matricula;
+        this.dataAniversario = dataAniversario;
+    }
+
+    public Long getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Long matricula) {
+        this.matricula = matricula;
+    }
+
+    public Date getDataAniversario() {
+        return dataAniversario;
+    }
+
+    public void setDataAniversario(Date dataAniversario) {
+        this.dataAniversario = dataAniversario;
+    }
+
+    public static boolean verificaMatricula(String matricula) {
+        if (matricula == null) {
+            return false;
+        } else if (matricula.trim().equals("")) {
+            return false;
+        } else if (matricula.length() != 8) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Aluno [matricula=" + matricula + ", dataAniversario="
+                + dataAniversario + "]";
+    }
+
+}
---
+++ src/tesp/victor/entidade/Professor.java
@@ -0,0 +1,28 @@
+package tesp.victor.entidade;
+
+import java.math.BigDecimal;
+
+public class Professor extends Pessoa {
+
+    private BigDecimal salario;
+    public static Double BONUS = 0.1D;
+
+    public Professor(Long id, String nome, String cpf, BigDecimal salario) {
+        super(id, nome, cpf);
+        this.salario = salario;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Professor [salario=" + salario + "]";
+    }
+
+}
